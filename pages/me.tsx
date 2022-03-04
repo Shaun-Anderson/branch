280b8f19a35d5be2a3bf64a8db50be6f164d5f9d
Shaun Anderson <shaun.anderson@outlook.com>
2022-03-04 15:32:26 +1100

    fix: dnd ssr

--- pages/me.tsx
+++ pages/me.tsx
@@ -1,21 +1,27 @@
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
-import Image, { ImageLoader } from "next/image";
+import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import {
-  DragDropContext,
-  Droppable,
-  Draggable,
-  NotDraggingStyle,
-  DraggingStyle,
-  DropResult,
-} from "react-beautiful-dnd";
+import { DropResult, resetServerContext } from "react-beautiful-dnd";
 import { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "../components/dnd";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGrip,
+  faGripVertical,
+  faEdit,
+  faTrashAlt,
+} from "@fortawesome/free-solid-svg-icons";
 
 interface Item {
   id: string;
   content: string;
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  resetServerContext();
+  return { props: {} };
+};
 
 // fake data generator
 const getItems = (count: number): Item[] =>
@@ -96,25 +102,87 @@ const Me: NextPage = () => {
 
         <p className="text my-2">This is an example of a bio</p>
 
+        <div className=" min-w-0 sm: w-96 max-w-lg">
+          <button className=" transition ease-in-out rounded-sm bg-teal-500 p-2 w-full text-white hover:scale-105 hover:bg-teal-600">
+            New link
+          </button>
+        </div>
+
         <DragDropContext onDragEnd={onDragEnd}>
           <Droppable droppableId="droppable">
             {(provided, snapshot): JSX.Element => (
-              <ul
-                className="flex flex-col mt-5"
+              <div
                 {...provided.droppableProps}
                 ref={provided.innerRef}
+                // style={getListStyle(snapshot.isDraggingOver)}
               >
                 {state.map((item, index) => (
                   <Draggable key={item.id} draggableId={item.id} index={index}>
                     {(provided, snapshot): JSX.Element => (
                       <div
-                        className="flex"
                         ref={provided.innerRef}
                         {...provided.draggableProps}
+                        className="flex"
+                        // style={getItemStyle(
+                        //   snapshot.isDragging,
+                        //   provided.draggableProps.style
+                        // )}
                       >
-                        {" "}
-                        <div {...provided.dragHandleProps}>handle</div>
+                        <div
+                          {...provided.dragHandleProps}
+                          className="flex items-center justify-center mr-5"
+                        >
+                          <FontAwesomeIcon
+                            className="w-5 h-5 text-gray-400"
+                            icon={faGripVertical}
+                          />
+                        </div>
                         <a
+                          href="https://nextjs.org/docs"
+                          className="transition ease-in-out m-1 w-96 max-w-full p-3 border bg-stone-100 border-stone-200 rounded-md flex hover:scale-105"
+                          // style={getItemStyle(
+                          //   snapshot.isDragging,
+                          //   provided.draggableProps.style
+                          // )}
+                        >
+                          <Image
+                            loader={myLoader}
+                            src="me.png"
+                            alt="Picture of the author"
+                            className="rounded-full"
+                            width={50}
+                            height={50}
+                          />
+                          <div className="ml-5">
+                            <h2 className="text-lg">{item.content}</h2>
+                            <p className="text">optional info about the link</p>
+                          </div>
+                        </a>
+                        <div className="align-center">
+                          <button className=" h-9 w-9 p-2 rounded-sm hover:bg-amber-100">
+                            <FontAwesomeIcon
+                              className="w-4 h-4 text-amber-500"
+                              icon={faEdit}
+                            />
+                          </button>
+                          <button className=" h-9 w-9 p-2 rounded-sm hover:bg-red-100">
+                            <FontAwesomeIcon
+                              className="w-4 h-4 text-red-500"
+                              icon={faTrashAlt}
+                            />
+                          </button>
+                        </div>
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+        {/* <ul>
+<a
                           href="https://nextjs.org/docs"
                           className="transition ease-in-out w-96 max-w-full p-3 border bg-stone-100 border-stone-200 rounded-md flex hover:scale-105"
 
@@ -136,33 +204,6 @@ const Me: NextPage = () => {
                             <p className="text">optional info about the link</p>
                           </div>
                         </a>
-                      </div>
-                    )}
-                  </Draggable>
-                ))}
-                {provided.placeholder}
-              </ul>
-            )}
-          </Droppable>
-        </DragDropContext>
-        {/* <ul>
-          <a
-            href="https://nextjs.org/docs"
-            className="transition ease-in-out w-96 max-w-full p-3 border bg-stone-100 border-stone-200 rounded-md flex hover:scale-105"
-          >
-            <Image
-              loader={myLoader}
-              src="me.png"
-              alt="Picture of the author"
-              className="rounded-full"
-              width={50}
-              height={50}
-            />
-            <div className="ml-5">
-              <h2 className="text-lg">This is a link title</h2>
-              <p className="text">optional info about the link</p>
-            </div>
-          </a>
         </ul> */}
       </main>
     </div>
