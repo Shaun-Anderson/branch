f4c11ff9a95a9bf0c06bb7622f5abc89fab64607
Shaun Anderson <shaun.anderson@outlook.com>
2022-03-27 23:19:11 +1100

    refactor: updated text colors

--- components/Button.tsx
+++ components/Button.tsx
@@ -12,7 +12,7 @@ interface ButtonProps {
 const Button = (props: ButtonProps) => {
   return (
     <button
-      className="inline-flex items-center justify-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-white bg-indigo-500 hover:bg-indigo-400 transition ease-in-out duration-150 cursor-pointer disabled:cursor-not-allowed disabled:bg-indigo-300"
+      className="inline-flex items-center justify-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-indigo-900 bg-indigo-100 hover:bg-indigo-200 transition ease-in-out duration-150 cursor-pointer disabled:cursor-not-allowed disabled:text-indigo-300 disabled:bg-indigo-50"
       type="submit"
       disabled={props.disabled}
     >
