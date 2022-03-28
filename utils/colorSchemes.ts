d92dc72534673c37f86e182d040d704c4b1b1201
Shaun Anderson <shaun.anderson@outlook.com>
2022-03-28 21:52:39 +1100

    refactor: added none option as index 0

--- utils/colorSchemes.ts
+++ utils/colorSchemes.ts
@@ -1,4 +1,5 @@
 export enum colorScheme {
+  "None" = "",
   "Default" = "bg-white",
   "Sea" = "bg-gradient-to-r from-cyan-500 to-blue-500",
   "Fire" = "bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500",
