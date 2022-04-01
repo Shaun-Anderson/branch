3c42a1c45621d19873e63c418907a0232dd1252e
Shaun Anderson <shaun.anderson@outlook.com>
2022-04-01 22:08:34 +1100

    feat: updated schemes

--- utils/colorSchemes.ts
+++ utils/colorSchemes.ts
@@ -1,10 +1,22 @@
 export enum colorScheme {
   "None" = "",
-  "Default" = "bg-white",
-  "Sea" = "bg-gradient-to-r from-cyan-500 to-blue-500",
-  "Fire" = "bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500",
+  "Default" = "bg-white text-black",
+  "Sea" = "bg-gradient-to-r from-cyan-500 to-blue-500 text-black",
+  "Fire" = "bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-black",
+  "Onix" = "bg-gray-800 text-white",
 }
 
 export enum linkColorScheme {
-  "Default" = "text-black bg-gray-500",
+  "None" = "",
+  "Light" = "text-black bg-stone-50",
+  "Regal" = "text-white bg-regal-blue",
+  "Dark" = "text-white bg-black",
+}
+
+export enum linkRounding {
+  "None" = "",
+  "Small" = "rounded-sm",
+  "Medium" = "rounded-md",
+  "Large" = "rounded-xl",
+  "Extra Large" = "rounded-full",
 }
